0ce1028c5326a28cdc61d249388bae967d895b37
Milos Ilic <milosilic89@outlook.com>
2023-07-09 21:16:06 +0200

    Created Components skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import './App.css';
-
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴Far Away🎒</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em> 🎒 You have X items on your list, and you already packef X (X%)</em>
+    </footer>
+  );
+}
