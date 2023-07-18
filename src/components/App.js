c35710167136215187913526ed791e236cd53a1d
Milos Ilic <milosilic89@outlook.com>
2023-07-18 13:31:30 +0200

    Removed Socks from initialItems

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import Stats from './Stats';
 
 export const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: true },
 ];
 
 export default function App() {
