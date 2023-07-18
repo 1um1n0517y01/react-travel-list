925219fd417aef91a1bcf19f0bbed14dd773eda2
Milos Ilic <milosilic89@outlook.com>
2023-07-18 13:26:14 +0200

    Moving Components Into Separate Files

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import Logo from './Logo';
+import Form from './Form';
+import PackingList from './PackingList';
+import Stats from './Stats';
+
+export const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+];
+
+export default function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleAddItems(item) {
+    // Adding items without mutating the initial array by spreading the items
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    // Deleting items without mutating the initial array by filtering
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    // Updating items without mutating the initial array by using map
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearingList() {
+    const confirmation = window.confirm(
+      'Are you sure you want to delete all items from the list?'
+    );
+    if (confirmation) setItems([]);
+  }
+
+  return (
+    <div className='app'>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearingList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,27 @@
+import { initialItems } from './App';
+
+export default function Stats({ items }) {
+  if (items.length <= initialItems.length) {
+    return (
+      <footer className='stats'>
+        <em>Start adding some items to your packing list 🌴</em>
+      </footer>
+    );
+  }
+
+  const numItems = items.length;
+  // Filter returns an array so we can use length
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className='stats'>
+      <em>
+        {percentage !== 100
+          ? `🎒 You have ${numItems} items on your list, and you already packed
+          ${numPacked} (${percentage}%)`
+          : `You got everything! Ready to go ✈️`}
+      </em>
+    </footer>
+  );
+}
